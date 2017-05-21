6e938b7dca7119a15be13394005d28cdc298ddcf
june <june@mazing.com>
2017-05-21 19:26:19 +0800

    测试

--- server/src/home/controller/index.js
+++ server/src/home/controller/index.js
@@ -6,6 +6,11 @@ module.exports = class extends Base {
   }
 
   async indexAction() {
+    const key = 'vtfg87h9ionjbhuygy87h9';
+    const ok = await redis.set(key, key);
+    console.log(`redis set: ${ok}`);
+    const result = await redis.get(key);
+    console.log(`redis get: ${result}`);
     return this.success('work hard! play hard!');
   }
 };
