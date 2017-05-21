e62489ef0dd25f0479ae4e70f728f38f05c2319d
june <june@mazing.com>
2017-05-21 17:21:03 +0800

    恢复单点模式

--- server/src/common/config/env/development.js
+++ server/src/common/config/env/development.js
@@ -12,26 +12,12 @@ module.exports = {
   // redis配置
   ioredis: {
     // 运行模式：single-单点模式；cluster-集群模式
-    mode: 'cluster',
-    hosts: [{
+    mode: 'single',
+    hosts: {
       host: '127.0.0.1',
-      port: 7000,
-    }, {
-      host: '127.0.0.1',
-      port: 7001,
-    }, {
-      host: '127.0.0.1',
-      port: 7002,
-    }, {
-      host: '127.0.0.1',
-      port: 7003,
-    }, {
-      host: '127.0.0.1',
-      port: 7004,
-    }, {
-      host: '127.0.0.1',
-      port: 7005,
-    }],
+      port: 6379,
+      password: 'admin',
+    },
   },
   // 微信SDK
   wechatSDK: {
