fa6d15258ea512063eea51d3b6cb606bd6e9fd0a
june <june@mazing.com>
2017-05-21 12:34:53 +0800

    修复eslint错误

--- server/src/common/thirdparty/wechat/wechat.js
+++ server/src/common/thirdparty/wechat/wechat.js
@@ -33,13 +33,17 @@ module.exports = {
    * @param data 微信小程序登录数据
    */
   async wxLoginDataDataDecrypt(data) {
-    let {sessionKey} = await this.getSessionKeyByCode(data.code);
+    let { sessionKey } = await this.getSessionKeyByCode(data.code);
     if (!think.isEmpty(sessionKey.errcode)) {
       return {};
     }
 
     // 解析出来的结构
-    // {"session_key":"mbmix9T8JLbRvBesHr0QWw==","expires_in":7200,"openid":"om0oZ0R0ESHEW_2_d-2nZTOy1AsM"}
+    // {
+    //    "session_key":"mbmix9T8JLbRvBesHr0QWw==",
+    //    "expires_in":7200,
+    //    "openid":"om0oZ0R0ESHEW_2_d-2nZTOy1AsM"
+    // }
     sessionKey = new Buffer(sessionKey.sessionk_ley, 'base64');
     const encryptedData = new Buffer(data.encryptedData, 'base64');
     const iv = new Buffer(data.iv, 'base64');
