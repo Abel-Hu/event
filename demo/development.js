5b5a5434b37dd8619d19d8e58f0cc9b5c0cb81c8
june <june@mazing.com>
2017-05-22 22:52:28 +0800

    玩一下thinkjs3.0

--- demo/development.js
+++ demo/development.js
@@ -1,16 +1,9 @@
 const Application = require('thinkjs');
-const babel = require('think-babel');
-const watcher = require('think-watcher');
-const notifier = require('node-notifier');
+const path = require('path');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
-  // watcher: watcher,
-  // transpiler: [babel, {
-  //   presets: ['think-node']
-  // }],
-  // notifier: notifier.notify.bind(notifier),
-  env: 'development'
+  env: __filename.replace(`${__dirname}${path.sep}`, '').replace('.js', ''),
 });
 
 instance.run();
--- demo/production.js
+++ demo/production.js
@@ -1,9 +1,10 @@
 const Application = require('thinkjs');
+const path = require('path');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
-  proxy: true, // use proxy
-  env: 'production'
+  proxy: true,
+  env: __filename.replace(`${__dirname}${path.sep}`, '').replace('.js', ''),
 });
 
 instance.run();
