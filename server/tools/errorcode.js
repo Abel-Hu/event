ea868e523cce37e591dc491bf26bd4d2abd65666
june <june@mazing.com>
2017-05-22 10:45:10 +0800

    自动生成错误码文档自动提交

--- server/tools/errorcode.js
+++ server/tools/errorcode.js
@@ -69,8 +69,8 @@ if (lastErrorCode !== thisErrorCode) {
   fs.writeFileSync(filename, content);
 
   // 提交到github
-  const process = require('child_process');
-  process.exec('ifconfig', function (error, stdout, stderr) {
+  const exec = require('child_process').exec;
+  exec(`cd ${path.dirname(filename)} && dir && git add . && git commit -m 自动生成错误码文档 && git push`, function (error, stdout) {
     console.log(stdout);
   });
 }
