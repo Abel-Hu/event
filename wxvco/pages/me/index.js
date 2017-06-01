a37fa3992027097ea90649a369cefda2fec88e80
ckken@qq.com <ck666666>
2017-06-01 23:56:18 +0800

    重构小程序

--- wxvco/pages/me/index.js
+++ wxvco/pages/me/index.js
@@ -2,7 +2,7 @@
  * Created by ken on 2017/5/31.
  */
 const {page} = wx.vco
-const o =  {
+const o = {
   onShow () {
     this.props.todo.utxt()
   },
@@ -11,8 +11,18 @@ const o =  {
     this.props.todo.ut()
   },
 
-  handleClick (e) {
+  uploadAvatar (e) {
     console.log(e)
+    wx.chooseImage().then((chooseData) => {
+      var tempFilePaths = chooseData.tempFilePaths
+      wx.uploadFile({
+        url: 'https://testevent.ruanzhijun.cn/api/system/upload', //仅为示例，非真实的接口地址
+        filePath: tempFilePaths[0],
+        name: 'file'
+      }).then((uploadData) => {
+        console.log(uploadData)
+      })
+    })
   },
 
   onLoad () {
@@ -20,28 +30,6 @@ const o =  {
       title: '我'
     })
     this.props.member.getMember()
-    /*    wx.chooseImage({
-     fail: function(res) {
-     console.log(res);
-     },
-     complete: function(res) {
-     console.log(res);
-     },
-     success: function(res) {
-     console.log(res);
-     // var tempFilePaths = res.tempFilePaths
-     // wx.uploadFile({
-     //   url: 'https://testevent.ruanzhijun.cn/api/system/upload', //仅为示例，非真实的接口地址
-     //   filePath: tempFilePaths[0],
-     //   name: 'file',
-     //   success: function(res){
-     //     var data = res.data
-     //     console.log(data);
-     //     //do something
-     //   }
-     // })
-     }
-     })*/
   }
 }
 page(o, {todo: 'todo', member: 'wechat/member'})
