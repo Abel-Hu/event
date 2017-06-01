e002012ddc7baabc368159639745544ec7ca00d9
ckken@qq.com <ck666666>
2017-06-02 01:03:43 +0800

    重构小程序

--- wxvco/app.js
+++ wxvco/app.js
@@ -4,9 +4,9 @@ const o = {
   onLaunch() {
     this.props.member.getMember(() => {
       this.props.system.checkToken((data) => {
-        if (data.code === 200002) {
+        if (data.code === 200002) { // 如果过期 重新登录
           this.props.member.getMember(true)
-        } else if (data.token) {
+        } else if (data.token) { // 更新 token
           this.props.member.updateToken(data.token)
         }
       })
