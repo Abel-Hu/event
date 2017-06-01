be08c0035302fa3c5d2ba7eee0080035bca6d8f8
iGenMO <331993917@qq.com>
2017-06-01 23:43:39 +0800

    1.代码规范

--- wxvco/pages/me/event/create/index.js
+++ wxvco/pages/me/event/create/index.js
@@ -1,74 +1,23 @@
 // index.js
-Page({
-
+const {page} = wx.vco
+const o =  {
   /**
    * 页面的初始数据
    */
   data: {
     item: [{title: '活动标题', value:'', placeholder: '请输入活动标题'},
-           {title: '活动图片', value:'', placeholder: '请上传活动图片'},
-           {title: '活动描述', value:'', placeholder: '请输入活动描述'},
-           {title: '开始时间', value:'', placeholder: '请输入开始时间'},
-           {title: '报名截止时间', value:'', placeholder: '请输入报名截止时间'},
-           {title: '最大参与人数', value:'', placeholder: '请输入最大参与人数'},
-           {title: '地址', value:'', placeholder: '请在地图上标出位置'},]
+      {title: '活动图片', value:'', placeholder: '请上传活动图片'},
+      {title: '活动描述', value:'', placeholder: '请输入活动描述'},
+      {title: '开始时间', value:'', placeholder: '请输入开始时间'},
+      {title: '报名截止时间', value:'', placeholder: '请输入报名截止时间'},
+      {title: '最大参与人数', value:'', placeholder: '请输入最大参与人数'},
+      {title: '地址', value:'', placeholder: '请在地图上标出位置'},]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad () {
     wx.setNavigationBarTitle({
       title: '发布活动'
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
-})
+}
+page(o)
