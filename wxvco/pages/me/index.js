072acc3a466bb18cf36155fe1ba1439a408e1949
iGenMO <331993917@qq.com>
2017-06-03 06:34:52 +0800

    1.发布活动 80% 2.活动列表 50%

--- wxvco/pages/me/index.js
+++ wxvco/pages/me/index.js
@@ -22,11 +22,14 @@ const o = {
   },
 
   onLoad () {
+
     wx.chooseLocation().then((uploadData) => {
       console.log(uploadData)
     }).catch((e) => {
       console.error(e)
     })
+
+
 
     wx.setNavigationBarTitle({
       title: '我'
