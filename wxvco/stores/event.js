6c84fb9c16cad91215920b2935cd8e4f5d92e3a2
ckken@qq.com <ck666666>
2017-06-04 18:53:39 +0800

    完成上传

--- wxvco/stores/event.js
+++ wxvco/stores/event.js
@@ -6,27 +6,66 @@ const Api = {
   publish: '/api/event/publish',
   update: '/api/event/update',
   info: '/api/event/info',
-  list: '/api/event/list'
+  list: '/api/event/list',
+  fav: '/api/event/fav',
+  unfav: '/api/event/unfav'
 }
 module.exports = class extends Store {
   state () {
     return {
       list: {
-        data: []
+        data: [],
+        headSequence: '',
+        lastSequence: ''
       },
       form: {},
       detail: {}
     }
   }
 
-  getList () {
-    http.get(Api.list).then((d)=>{
-      console.log(d)
+  getList (reload = false, cb) {
+    let params = {
+      pageSize: 10
+    }
+    if (reload) {
+      params.headSequence = this.list.headSequence
+    } else if (this.list.lastSequence) {
+      params.lastSequence = this.list.lastSequence
+    } else if (this.list.lastSequence === false) {
+      return cb && cb()
+    }
+
+    http.get(Api.list, params).then((d) => {
+      const {list, lastSequence, headSequence} = d
+      if (!reload) {
+        this.list.data = this.list.data.concat(list)
+        this.list.lastSequence = lastSequence || false
+        this.list.headSequence = headSequence
+      } else {
+        list.map(d => {
+          this.list.data.unshift(d)
+        })
+      }
+      cb && cb()
+    })
+  }
+
+  fav (eventId, index, cb) {
+    http.post(Api.fav, {eventId}).then(d => {
+      this.list.data[index].isFav = true
+      cb && cb()
+    })
+  }
+
+  unfav (eventId, index, cb) {
+    http.post(Api.unfav, {eventId}).then(d => {
+      this.list.data[index].isFav = false
+      cb && cb()
     })
   }
 
   publish (data) {
-    http.post(Api.publish, data).then((d)=>{
+    http.post(Api.publish, data).then((d) => {
       console.log(d)
     })
   }
